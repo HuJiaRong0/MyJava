51fe14453175105273b89bed098f67f5940b73a2
HuJiaRong0 <77434091+HuJiaRong0@users.noreply.github.com>
2021-01-14 20:52:36 +0800

    练习1

--- src/test1/Demo1.java
+++ src/test1/Demo1.java
@@ -1,4 +1,13 @@
 package test1;
 
+import java.util.Scanner;
+
 public class Demo1 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y;
+        y=(x+11*x+111*x+1111*x)*2+11111*x;
+        System.out.println(x+"+"+11*x+"+"+111*x+"+"+1111*x+"+"+11111*x+"+"+1111*x+"+"+111*x+"+"+11*x+"+"+x+"="+y);
+    }
 }
