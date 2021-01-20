2bab1eaab8818690c2a1d0aa334b9988ce0c4d7e
HuJiaRong0 <77434091+HuJiaRong0@users.noreply.github.com>
2021-01-20 21:12:05 +0800

    练习7

---
+++ src/test7/Demo7.java
@@ -0,0 +1,13 @@
+package test7;
+
+public class Demo7 {
+    public static void main(String[]args){
+        int x=451;
+        int y=369;
+        String z;
+        z=(x<y)?"通过一本录取分数线":"未通过一本录取分数线";
+        System.out.println("您的高考总分: "+y);
+        System.out.println("第一批本科录取分数线:  "+x);
+        System.out.println("高考结果："+z);
+    }
+}
