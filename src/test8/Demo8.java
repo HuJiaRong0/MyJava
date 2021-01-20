1bf92c35d773e5a81ba6de13159878a41ba6d091
HuJiaRong0 <77434091+HuJiaRong0@users.noreply.github.com>
2021-01-20 21:30:01 +0800

    练习8

---
+++ src/test8/Demo8.java
@@ -0,0 +1,12 @@
+package test8;
+
+import java.math.BigInteger;
+
+public class Demo8 {
+    public static void main(String[]args){
+        BigInteger x=new BigInteger("7100000000");
+        long y=1400000000;
+        System.out.println("2019年世界人口总数约："+x);
+        System.out.println("2019年中国人口总数约："+y);
+    }
+}
