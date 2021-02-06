7fb93cc0644756f3f90095ef4232d7155b2ee7e8
HuJiaRong0 <77434091+HuJiaRong0@users.noreply.github.com>
2021-02-06 23:20:05 +0800

    练习7

--- src/test7/Demo7.java
+++ src/test7/Demo7.java
@@ -3,9 +3,9 @@ package test7;
 public class Demo7 {
     public static void main(String[]args){
         int x=451;
-        int y=369;
+        int y=451;
         String z;
-        z=(x<y)?"通过一本录取分数线":"未通过一本录取分数线";
+        z=(x<=y)?"通过一本录取分数线":"未通过一本录取分数线";
         System.out.println("您的高考总分: "+y);
         System.out.println("第一批本科录取分数线:  "+x);
         System.out.println("高考结果："+z);
