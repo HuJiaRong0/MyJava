f1b4459fb378dbf07698c0dd37a82b97bc1fad9f
HuJiaRong0 <77434091+HuJiaRong0@users.noreply.github.com>
2021-02-06 23:00:02 +0800

    练习23

--- src/test16/Demo16.java
+++ src/test16/Demo16.java
@@ -1,4 +1,38 @@
 package test16;
 
+import java.util.Scanner;
+
 public class Demo16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String temp = sc.next();
+        String[] arr = temp.split(",");
+        String code = arr[0];
+        int count = Integer.parseInt(arr[1]);
+        double price = Order.getOrderAmount(code, count);
+        System.out.println(price);
+    }
+}
+
+class Item {
+    String[] ItemCode = {"C_101", "C_102"};
+    String[] ItemName = {" HUAWEI Mate30 Pro", "XIAOMI 10 Pro"};
+    double[] UnitPrice ={5000.00, 4699.00};
+}
+
+
+class Order extends Item {
+
+    public static double getOrderAmount(String itemCode, int itemCount) {
+        Item item = new Item();
+        String[] ItemCode = item.ItemCode;
+        double[] UnitPrice = item.UnitPrice;
+        double price = 0;
+        for (int i = 0; i < ItemCode.length; i++) {
+            if (ItemCode[i].equals(itemCode)) {
+                price = itemCount * UnitPrice[i];
+            }
+        }
+        return price;
+    }
 }
--- src/test23/Demo23.java
+++ src/test23/Demo23.java
@@ -1,5 +1,47 @@
 package test23;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Demo23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        HashMap<Character,Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (map.containsKey(ch)){
+                int n = map.get(ch);
+                n++;
+                map.put(ch,n);
+            }else {
+                map.put(ch,1);
+            }
+        }
+        int max = -1;
+        ArrayList list = new ArrayList();
+        for (char ch: map.keySet()){
+            int n = map.get(ch);
+            if (n > max){
+                max = n;
+                list.clear();
+                list.add(ch);
+            }
+            if (n == max){
+                list.add(ch);
+            }
+
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (list.contains(ch)){
+                continue;
+            }
+            sb.append(ch);
+        }
+        System.out.println(sb.toString());
+    }
 
 }
